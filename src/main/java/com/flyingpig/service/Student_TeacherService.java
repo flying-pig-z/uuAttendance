beed05726ae477bf78ecabb52f6588c19586cfc5
flying_pig <1839976096@qq.com>
2023-09-17 09:12:49 +0800

    Delete images directory

--- src/main/java/com/flyingpig/service/Student_TeacherService.java
+++
@@ -1,9 +0,0 @@
-package com.flyingpig.service;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-
-
-public interface Student_TeacherService {
-    Integer selectTeacherByStudent(Integer studentId);
-}
--- src/main/java/com/flyingpig/service/SupervisionService.java
+++
@@ -1,7 +0,0 @@
-package com.flyingpig.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface SupervisionService {
-}
