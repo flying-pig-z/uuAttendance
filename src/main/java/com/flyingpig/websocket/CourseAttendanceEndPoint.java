118224113e92da3f1f7b3d266a8af020db84857a
flying_pig <1839976096@qq.com>
2023-09-21 08:42:31 +0800

    Delete images directory

--- src/main/java/com/flyingpig/websocket/CourseAttendanceEndPoint.java
+++ src/main/java/com/flyingpig/websocket/CourseAttendanceEndPoint.java
@@ -1,4 +1,4 @@
-package com.flyingpig.ws;
+package com.flyingpig.websocket;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.flyingpig.config.WebSocketConfig;
