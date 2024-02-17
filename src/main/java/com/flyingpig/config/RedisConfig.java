76b9afe5681584aa56eabce58ebb264a4e106d6d
flying_pig <1839976096@qq.com>
2024-02-17 16:37:24 +0800

    移动一些文件和删除一些无关文件

--- src/main/java/com/flyingpig/config/RedisConfig.java
+++ src/main/java/com/flyingpig/config/RedisConfig.java
@@ -1,6 +1,5 @@
 package com.flyingpig.config;
 
-import com.flyingpig.util.FastJsonRedisSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
